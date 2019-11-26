2ea2ed2129b323d2407e9330993511c96fc8b7f0
but-sergey <but-sergey@bk.ru>
2019-11-26 10:01:27 +0300

    Урок 7

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,9 +16,11 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
+
+            VerticalLine line2 = new VerticalLine(10, 20, 30, '+');
+            line2.Draw();
 
             Console.ReadLine();
         }
